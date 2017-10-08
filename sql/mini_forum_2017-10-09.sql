edb4f1650f4073ba6951e11d0b289a6cc8ec3636
Jamael Tanveer Nayon <imjtnayon@gmail.com>
2017-10-09 04:35:32 +0600

    add updated mysql file

--- sql/mini_forum_2017-10-09.sql
+++ sql/mini_forum_2017-10-09.sql
@@ -7,7 +7,7 @@
 #
 # Host: 127.0.0.1 (MySQL 5.5.5-10.1.21-MariaDB)
 # Database: mini_forum
-# Generation Time: 2017-10-07 21:43:09 +0000
+# Generation Time: 2017-10-08 22:34:32 +0000
 # ************************************************************
 
 
@@ -259,7 +259,21 @@ VALUES
 	(9,'That\'s just part of the story. That just does not destroy his heroism.',14,4,'2017-10-07 18:38:52','2017-10-07 18:38:52'),
 	(10,'Heroism?! big lol',14,2,'2017-10-07 18:39:32','2017-10-07 18:39:32'),
 	(17,'sad',14,2,'2017-10-07 21:39:29','2017-10-07 21:39:29'),
-	(18,'Are you kidding me?!',14,2,'2017-10-07 21:40:38','2017-10-07 21:40:38');
+	(18,'Are you kidding me?!',14,2,'2017-10-07 21:40:38','2017-10-07 21:40:38'),
+	(19,'<p><strong>yet again!</strong></p>',25,1,'2017-10-08 20:09:46','2017-10-08 20:09:46'),
+	(20,'<p>my name is&nbsp;<em>Nayon</em>&nbsp;</p>',25,1,'2017-10-08 20:10:28','2017-10-08 20:10:28'),
+	(21,'<p><strong>This&nbsp;</strong>is an&nbsp;<em>HTML&nbsp;<strong>Reply.</strong></em></p>',25,1,'2017-10-08 20:11:15','2017-10-08 20:11:15'),
+	(22,'<p>Its working&nbsp;<strong><em>well.</em></strong></p>',25,1,'2017-10-08 20:11:40','2017-10-08 20:11:40'),
+	(23,'hello world',25,1,'2017-10-08 20:24:23','2017-10-08 20:24:23'),
+	(24,'What happened?',25,1,'2017-10-08 20:30:29','2017-10-08 20:30:29'),
+	(25,'Wow',25,1,'2017-10-08 20:36:10','2017-10-08 20:36:10'),
+	(26,'No!',25,1,'2017-10-08 20:36:24','2017-10-08 20:36:24'),
+	(27,'This can\'t be true!',25,1,'2017-10-08 20:36:36','2017-10-08 20:36:36'),
+	(28,'you must be kidding me!',25,1,'2017-10-08 20:38:44','2017-10-08 20:38:44'),
+	(29,'Damn!',25,1,'2017-10-08 20:42:24','2017-10-08 20:42:24'),
+	(30,'<p>sad!</p>',14,1,'2017-10-08 20:48:00','2017-10-08 20:48:00'),
+	(31,'<p><strong>Really!</strong></p>',14,1,'2017-10-08 20:51:39','2017-10-08 20:51:39'),
+	(32,'<p>Reply&nbsp;<em>cant be&nbsp;<strong>smaller than&nbsp;</strong>two</em><strong> characters</strong></p>',14,1,'2017-10-08 21:11:59','2017-10-08 21:11:59');
 
 /*!40000 ALTER TABLE `replies` ENABLE KEYS */;
 UNLOCK TABLES;
@@ -296,7 +310,7 @@ VALUES
 	(4,'Trump on Facebook','Responding to President Trumpâ€™s tweet this week that â€œFacebook was always anti-Trump,â€ Mark Zuckerberg, the chief executive of Facebook, defended the company by noting that Mr. Trumpâ€™s opponents also criticize it â€” as having aided Mr. Trump. If everyone is upset with you, Mr. Zuckerberg suggested, you must be doing something right.',5,3,'2017-10-01 11:25:08','2017-10-01 11:25:08'),
 	(5,'Wall Streen','A $732 billion investor says Wall Street is seriously underestimating Trump\'s tax plan.',2,3,'2017-10-01 11:26:11','2017-10-01 11:26:11'),
 	(6,'Julian Assange in action','Julian Assange is rallying behind Catalan separatists ahead of a historic referendum â€” and Russia has taken notice.',5,3,'2017-10-01 11:26:54','2017-10-01 11:26:54'),
-	(7,'Volkswagen scandal','The diesel emissions cheating scandal will cost Volkswagen an extra $3bn (â‚¬2.5bn), because engines are proving \"far more technically complex and time consuming\" to adapt the company said.',2,2,'2017-10-01 11:29:18','2017-10-01 11:29:18'),
+	(7,'Volkswagen scandal','The diesel emissions cheating scandal will cost Volkswagen an extra $3bn (â?¬2.5bn), because engines are proving \"far more technically complex and time consuming\" to adapt the company said.',2,2,'2017-10-01 11:29:18','2017-10-01 11:29:18'),
 	(8,'Porche in India','The long-awaited Porsche 911 GT3 will finally set foot in India on October 9, 2017. The 911 series is all about blowing your mind wide open with its ridiculous specs. Unlike the Carrera or the Turbo, the GT3 is powered by the rumbly 4.0-litre flat-six.',3,1,'2017-10-01 11:31:07','2017-10-01 11:31:07'),
 	(9,'Electric car','More automakers are partnering in preparation for an electric car future. Mazda and Toyota are next, joining with Denso Corporation to form a new company that will develop electric vehicle technologies.',3,3,'2017-10-01 11:32:00','2017-10-01 11:32:00'),
 	(10,'Tesla','Tesla Model X the First SUV Ever to Achieve 5-Star Crash Rating in Every Category',3,2,'2017-10-01 11:34:13','2017-10-01 11:34:13'),
@@ -304,8 +318,8 @@ VALUES
 	(12,'Wonder Woman Story','Wonder Woman: Diana, princess of the Amazons, trained to be an unconquerable warrior. Raised on a sheltered island paradise, when a pilot crashes on their shores and tells of a massive conflict raging in the outside world, Diana leaves her home, convinced she can stop the threat. Fighting alongside man in a war to end all wars, Diana will discover her full powers and her true destiny.',4,3,'2017-10-01 11:39:25','2017-10-01 11:39:25'),
 	(13,'Guardian of the Galaxy Vol. 2','Guardians of the Galaxy Vol. 2: After saving Xandar from Ronan\'s wrath, the Guardians are now recognized as heroes. Now the team must help their leader Star Lord (Chris Pratt) uncover the truth behind his true heritage. Along the way, old foes turn to allies and betrayal is blooming. And the Guardians find that they are up against a devastating new menace who is out to rule the galaxy.',4,2,'2017-10-01 11:40:11','2017-10-01 11:40:11'),
 	(14,'Spider-Man: Homecoming Rating','Spider-Man: Homecoming: Thrilled by his experience with the Avengers, Peter returns home, where he lives with his Aunt May, under the watchful eye of his new mentor Tony Stark, Peter tries to fall back into his normal daily routine - distracted by thoughts of proving himself to be more than just your friendly neighborhood Spider-Man - but when the Vulture emerges as a new villain, everything that Peter holds most important will be threatened.',4,1,'2017-10-01 11:40:37','2017-10-01 11:40:37'),
-	(15,'Rome','Rome is a city thatâ€™s best discovered on foot, which is why itâ€™s so wonderful in the fall. September is considered part of shoulder season, so while you wonâ€™t see hordes of people, there will still be plenty of locals and tourists enjoying the weather and taking in the many sights of Rome. ',6,3,'2017-10-01 11:43:40','2017-10-01 11:43:40'),
-	(16,'San Diego','If you havenâ€™t been to San Diego yet, youâ€™re missing out on one of Californiaâ€™s best new food experiences. The city used to be known solely for its fish tacos, but now inventive chefs are offering oysters that have been raised specifically for a restaurant (Ironside Fish & Oyster) and incredibly delicious doughnuts (VG Donut & Bakery). The other beauty of spending a weekend in San Diego is the ability to take a day trip across the border to Tijuana, another city gaining a food reputation.',6,2,'2017-10-01 11:44:22','2017-10-01 11:44:22'),
+	(15,'Rome','Rome is a city thatâ??s best discovered on foot, which is why itâ??s so wonderful in the fall. September is considered part of shoulder season, so while you wonâ??t see hordes of people, there will still be plenty of locals and tourists enjoying the weather and taking in the many sights of Rome. ',6,3,'2017-10-01 11:43:40','2017-10-01 11:43:40'),
+	(16,'San Diego','If you havenâ??t been to San Diego yet, youâ??re missing out on one of Californiaâ??s best new food experiences. The city used to be known solely for its fish tacos, but now inventive chefs are offering oysters that have been raised specifically for a restaurant (Ironside Fish & Oyster) and incredibly delicious doughnuts (VG Donut & Bakery). The other beauty of spending a weekend in San Diego is the ability to take a day trip across the border to Tijuana, another city gaining a food reputation.',6,2,'2017-10-01 11:44:22','2017-10-01 11:44:22'),
 	(17,'Thailan','As far as honeymoon destinations go, Thailand is famous the world over for its gorgeous beaches and smiley people. The problem is that if you choose the wrong beach location, like Patong Beach, you\'re going to find yourself spending your honeymoon in a go-go bar.',6,2,'2017-10-01 11:45:56','2017-10-01 11:45:56'),
 	(18,'Maldives','Thank goodness the Drift Thelu Veliga Retreat, a new four-star resort in the Maldives, decided not to compete with Mother Nature for show-stopping fabulosity.\r\n\r\nRead more: http://www.dailymail.co.uk/travel/travelsupplement/article-4935914/This-Maldives-retreat-won-t-break-bank.html#ixzz4uFwcXoNJ \r\nFollow us: @MailOnline on Twitter | DailyMail on Facebook',6,1,'2017-10-01 11:46:45','2017-10-01 11:46:45'),
 	(19,'Moeen Ali talks about Ashes','England can win Ashes without Ben Stokes, says Moeen Ali.',8,1,'2017-10-01 11:48:27','2017-10-01 11:48:27'),
@@ -314,7 +328,10 @@ VALUES
 	(22,'Srilanka vs Pakistan','Eleven sessions into this match, with only four to go, what has been achieved? A three-run lead. That measly advantage belongs, if you are wondering, to Pakistan. They had begun the session in danger of conceding a lead of over 50, but thanks to Haris Sohail\'s dogged persistence, they stuck around long enough to limp over Sri Lanka\'s 419.',8,1,'2017-10-01 11:51:51','2017-10-01 11:51:51'),
 	(23,'India vs Australia','India wins over Australia in the ODI series by 4-1.',8,3,'2017-10-01 16:17:07','2017-10-01 16:17:07'),
 	(24,'Arsenal vs Brighton','Arsenal wins convincingly against Brighton by 2 goals.',8,3,'2017-10-01 18:02:22','2017-10-01 18:02:22'),
-	(25,'Test Cricket','South Africa won convincingly against Bangladesh in the first match of the series. Proteas humbled Bangladesh by a big margin of 333 runs.',8,1,'2017-10-02 20:50:57','2017-10-02 20:50:57');
+	(25,'Test Cricket','South Africa won convincingly against Bangladesh in the first match of the series. Proteas humbled Bangladesh by a big margin of 333 runs.',8,1,'2017-10-02 20:50:57','2017-10-02 20:50:57'),
+	(26,'First Topic','<p>This is a new topic created from the new forum!&nbsp;<strong>I am very happy about it!&nbsp;</strong><em>Hope that it works perfectly!</em></p>',1,1,'2017-10-08 21:49:04','2017-10-08 21:49:04'),
+	(27,'New Topic as an User','<p>A general should be able to create post without any problem. <strong>This</strong> is what i am trying at this moment. Hopefully it gets posted! <span style=\"text-decoration: underline;\"><em>BYE</em></span></p>',2,2,'2017-10-08 21:52:35','2017-10-08 21:52:35'),
+	(28,'Late night coding','<p>It\'s 4:32 at night. I have been spending sleepless nights for last two weeks. Hopefully it turns out well!</p>',2,3,'2017-10-08 22:32:52','2017-10-08 22:32:52');
 
 /*!40000 ALTER TABLE `topics` ENABLE KEYS */;
 UNLOCK TABLES;
